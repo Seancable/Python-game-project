c542e803f0d1f3b149f11152e0f1aae213e0b289
Seancable <93600339+Seancable@users.noreply.github.com>
2023-02-28 14:11:16 +0000

    Add files via upload

--- character.py
+++ character.py
@@ -19,9 +19,12 @@ class Character:
         self.height=IMAGE.get_height()
         self.rows=(self.height/self.row)/2
         self.cols=(self.width/self.row)/2
+        self.health=10
+        self.length=(WIDTH/20)*self.health
         
     def draw(self,canvas):
         canvas.draw_image(IMAGE, (self.cols, self.rows), (IMAGE.get_width()/self.col, IMAGE.get_height()/self.row), self.pos.get_p(), (100,100))
+        canvas.draw_line((0,0),(self.length,0),30,'Red')
     def next_frame(self):
         self.cols+=IMAGE.get_width()/self.col
         if self.cols>=self.width:
@@ -36,7 +39,20 @@ class Character:
             self.pos.add(Vector(WIDTH,0))
         if self.pos.get_p()[0]>WIDTH:
             self.pos.subtract(Vector(WIDTH,0))
+        if self.pos.get_p()[1]<HEIGHT-100:
+            self.pos.add(Vector(0,5))
+            val=int(self.pos.get_p()[1])
+            val=float(val)
+            if val==HEIGHT-100:
+                self.pos.subtract(Vector(0,self.pos.get_p()[1]))
+                self.pos.add(Vector(0,val))
 
+    def on_ground(self):
+        if round(self.pos.get_p()[1],-1)==HEIGHT-100:
+            return True
+        else:
+            return False
+                
 class Clock:
     def __init__(self):
         self.time=0
@@ -52,22 +68,29 @@ class Keyboard:
         self.right = False
         self.left = False
         self.up=False
+        self.fire=False
 
     def keyDown(self, key):
         if key == simplegui.KEY_MAP['right']:
             self.right = True
         if key == simplegui.KEY_MAP['left']:
             self.left = True
-        if key == simplegui.KEY_MAP['up']:
+        if key == simplegui.KEY_MAP['space']:
             self.up = True
-
+        if key == simplegui.KEY_MAP['F']:
+            self.fire =  True
+            
+ 
     def keyUp(self, key):
         if key == simplegui.KEY_MAP['right']:
             self.right = False
         if key == simplegui.KEY_MAP['left']:
             self.left = False
-        if key == simplegui.KEY_MAP['up']:
+        if key == simplegui.KEY_MAP['space']:
             self.up = False
+        if key == simplegui.KEY_MAP['F']:
+            self.fire = True
+            
 
 class Interaction:
     def __init__(self, sheet, keyboard):
@@ -84,11 +107,18 @@ class Interaction:
             self.sheet.vel.add(Vector(-1,0))
             self.sheet.rows=(self.sheet.height/self.sheet.row/2)*3
             self.sheet.width=(IMAGE.get_width()/self.sheet.col)*6
+        elif self.keyboard.up:
+            if self.sheet.on_ground()==True:
+                self.sheet.width=(IMAGE.get_width()/self.sheet.col)*3
+                if self.sheet.cols>=self.sheet.width:
+                    self.sheet.cols=(IMAGE.get_width()/self.sheet.col)/2
+                self.sheet.vel.add(Vector(0,-10))
+                self.sheet.rows=(self.sheet.height/self.sheet.row/2)*7
+                self.sheet.width=(IMAGE.get_width()/self.sheet.col)*3
         else:
             self.sheet.rows=(self.sheet.height/self.sheet.row)/2
             self.sheet.width=IMAGE.get_width()
-    
-                
+                               
 sheet=Character(Vector(WIDTH/2,HEIGHT-100))
 clock=Clock()
 kbd=Keyboard()
