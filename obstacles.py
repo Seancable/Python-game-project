a3aebce12bed125025571454981f7bee869067e7
AlexanderDan10 <125551412+AlexanderDan10@users.noreply.github.com>
2023-03-12 12:35:47 +0000

    Update obstacles.py

--- obstacles.py
+++ obstacles.py
@@ -1,21 +1,35 @@
-from vector import Vector
-try:
-    import simplegui
-except ImportError:
-    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
-
-
 class Obstacle:
-    def __init__(self, x, y, length, height, thickness, colour):
+    def __init__(self, x, y, endX, endY, thickness, colour):
         self.x = x
         self.y = y
-        self.location = (self.x, self.y)
-        self.length = length
-        self.height = height
+        self.start = (self.x, self.y)
+        self.endX = endX
+        self.endY = endY
+        self.end = (self.endX, self.endY)
         self.thickness = thickness
         self.colour = colour
 
     def draw(self, canvas):
-        canvas.draw_line(self.location, (self.length, self.height), self.thickness, self.colour)
+        canvas.draw_line(self.start, (self.endX, self.endY), self.thickness, self.colour)
+        
+    def getStartX(self):
+        return self.x
+
+    def getEndX(self):
+        return self.endX
+
+    def getY(self):
+        return self.y
+
+    def collisions(self, character):
+        #detects collisions between obstacles and character
+        #could also be used for enemy if needed
+        if self.x < character.pos.get_p()[0] < self.endX:
+            if round(character.pos.get_p()[1]) + 35 < self.y < round(character.pos.get_p()[1]) + 50:
+                return True
+            #elif round(character.pos.get_p()[1]) < self.y < round(character.pos.get_p()[1]) + 15: #added the 15 bcs not always detecting collisions, program not running often enough
+             #   return True
+        return False
+
         
         
