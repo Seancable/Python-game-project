43fd87751d44dc5e0164d71a23c2ae894f901d44
wp590 <122525912+wp590@users.noreply.github.com>
2023-03-16 20:45:36 +0000

    Update menu.py

--- menu.py
+++ menu.py
@@ -19,5 +19,14 @@ class Menu:
         #canvas.draw_polygon([[],[],[],[], 5, 'Silver', 'Silver']) # Settings Button
         #canvas.draw_polygon([[],[],[],[], 5, 'Silver', 'Silver']) # Exit Button
 
+        menChoice = "start"
+
+        return menChoice
+
     def pausemenu(self, canvas):
-        print("sus")
+        canvas.draw_polygon([[self.width/2 - 200, self.height/2 - 150], [self.width/2 + 200, self.height/2 - 150],
+        [self.width/2 + 200, self.height/2 +  150], [self.width/2 - 200, self.height/2 + 150]], 12, 'Black', 'ForestGreen')
+        #canvas.draw_polygon([[],[],[],[], 5, 'Silver', 'Silver']) # Continue Button
+        #canvas.draw_polygon([[],[],[],[], 5, 'Silver', 'Silver']) # Load Game Button ** If time allows**
+        #canvas.draw_polygon([[],[],[],[], 5, 'Silver', 'Silver']) # Settings Button
+        #canvas.draw_polygon([[],[],[],[], 5, 'Silver', 'Silver']) # Exit Button
