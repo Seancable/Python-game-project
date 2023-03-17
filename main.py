147e8c9bdd631ebc12857301b5b192550db49ea4
AlexanderDan10 <125551412+AlexanderDan10@users.noreply.github.com>
2023-03-17 16:10:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,113 +5,89 @@ from bullet import Bullet
 from keyboard import Keyboard
 from enemy import EnemyT1
 from interaction import *
-from obstacle import Obstacle
+from obstacles import Obstacle
 from healthpack import HealthPack
 from vector import Vector
 
-try:
-    import simplegui
-except ImportError :
-    import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
+enemy1_a = simplegui.load_image('https://s3.us-east-2.amazonaws.com/ascensiongamedev/filehost/b28a06ac5850e6556ec12c44f65b3e14.png')
+enemy1_b = simplegui.load_image('https://s3.us-east-2.amazonaws.com/ascensiongamedev/filehost/55942801df7a044914b713a281c53c7d.png')
+enemy1_c = simplegui.load_image('https://s3.us-east-2.amazonaws.com/ascensiongamedev/filehost/16eeaa1a8b6b77bb7fbbd21284a820eb.png')
+bck = simplegui.load_image('https://raw.githubusercontent.com/Seancable/Python-game-project/main/Level1_01.png')
 
 class Main:
+    def __init__(self, obs, character, background, clock, kbd, hp, inter, gun, bullet, enemyList, enemy, btime):
+        self.obs = obs
+        self.character = character
+        self.background = background
+        self.clock = clock
+        self.kbd = kbd
+        self.hp = hp
+        self.inter = inter
+        self.gun = False
+        self.bullet = bullet
+        self.enemyList = enemyList
+        self.enemy = enemy
+        self.btime = 0
+        self.x = 0
 
-    def __init__(self):
-        pass
+    def runGame(self, canvas):
+        self.clock.tick()
+        self.inter.update()
+        self.character.update()
+        self.background.draw(canvas)
+        self.character.draw(canvas)
+        self.enemy.draw(canvas)
+        self.bullet.draw(canvas)
+        self.hp.draw(canvas)
+        for obs in self.obs:
+            obs.draw(canvas)
+        self.btime+=1
+        if self.clock.transistion(10)==True:
+            self.character.next_frame()
+        if self.kbd.fire and self.bullet.is_fired():
+            self.bullet.pos=Vector(self.character.pos.get_p()[0], self.character.pos.get_p()[1])
+            self.bullet.draw(canvas)
+            self.gun=True
+            self.btime=0
+        if self.gun==True:
+            self.bullet.update()
+            if self.btime==50:
+                self.gun=False
+                self.bullet.pos=Vector(0,0)
 
-    def begin(t):
-        print("Game start at", time.ctime(t))
-        return 0
-
-WIDTH = 500
-HEIGHT = 500
-
-
-ma = Main
-tm = time.time()
-startType = ma.begin(tm)
-
-global gun,btime, kbd
+kbd=Keyboard()
+check = False
 y1 = 350
 y2 = 250
 y3 = 150
 y4 = 50
 obs = [Obstacle(100, y1, 400, y1, 20, "Orange"),
-       Obstacle(200, y2, 550, y2, 20, "Orange"),
-       Obstacle(50, y3, 150, y3, 20, "Orange"),
-       Obstacle(280, y4, 55, y4, 20, "red")]
+        Obstacle(200, y2, 550, y2, 20, "Orange"),
+        Obstacle(50, y3, 300, y3, 20, "Orange"),
+        Obstacle(55, y4, 280, y4, 20, "red")]      
 sheet=Character(Vector(WIDTH/2,HEIGHT-100), obs)
 background = Background(bck, WIDTH, HEIGHT)
 clock=Clock()
-kbd=Keyboard()
 hp=HealthPack()
 inter=Interaction(sheet,kbd,hp, obs)
+gun=False
 bullet=Bullet(sheet)
-enemyt1list = [enemy1_a, enemy1_b, enemy1_c]
+enemyList = [enemy1_a, enemy1_b, enemy1_c]
 i = random.randint(0,2)
-enemyt1=EnemyT1(enemyt1list[i], (Vector(450, 400)))
-
-
+enemy=EnemyT1(enemyList[i], (Vector(450, 400)))
+btime=0
+main = Main(obs, sheet, background, clock, kbd, hp, inter, gun, bullet, enemyList, enemy, btime)
 
 def draw(canvas):
-    btime=0
-    gun=False
-    background.draw(canvas)
-    sheet.draw(canvas)
+    global main
+    main.runGame(canvas)
 
-    clock.tick()
-    inter.update()
-    sheet.update()
-    enemyt1.draw(canvas)
-    bullet.draw(canvas)
-    hp.draw(canvas)
-    for obstacle in obs:
-        obstacle.draw(canvas)
-    btime+=1
-    if clock.transistion(10)==True:
-        sheet.next_frame()
-    if kbd.fire and bullet.is_fired():
-        bullet.pos=Vector(sheet.pos.get_p()[0],sheet.pos.get_p()[1])
-        bullet.draw(canvas)
-        gun=True
-        btime=0
-    if gun==True:
-        bullet.update()
-        if btime==50:
-            gun=False
-            bullet.pos=Vector(0,0)
-
-def start(canvas):
-    frame.set_canvas_background('Grey')
-    mn = Menu(WIDTH, HEIGHT)
-    choice = mn.mainmenu(canvas)
-
-    if choice == "start":
-        print("No one would have believed ... ")
-
-        draw(canvas)
-
-    else:
-        draw(canvas)
-
-
-def cont(canvas):
-    frame.set_canvas_background('Grey')
-    mn = Menu(WIDTH, HEIGHT)
-    mn.pausemenu(canvas)
-
-
-# Create a frame and assign the callback to the event handler
-kbd=Keyboard()
-frame = simplegui.create_frame(" The War of The Worlds ", WIDTH, HEIGHT)
-if startType == 0:
-
-    frame.set_draw_handler(start)
-else:
-    frame.set_draw_handler(cont)
-
+frame = simplegui.create_frame('Testing', WIDTH, HEIGHT)
 frame.set_draw_handler(draw)
 frame.set_keydown_handler(kbd.keyDown)
 frame.set_keyup_handler(kbd.keyUp)
-# Start the frame animation
 frame.start()
+
+
+                
+
