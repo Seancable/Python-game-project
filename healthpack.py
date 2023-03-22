46c43c9a332f1c3f26ec883a06365b9128b74a2c
Seancable <93600339+Seancable@users.noreply.github.com>
2023-03-22 15:16:31 +0000

    Update healthpack.py

--- healthpack.py
+++ healthpack.py
@@ -9,15 +9,16 @@ HEIGHT=500
 IMAGE=simplegui.load_image('https://opengameart.org/sites/default/files/preview_843.png')
 
 class HealthPack:
-    def __init__(self):
+    def __init__(self,pos):
         self.height=IMAGE.get_height()
         self.width=IMAGE.get_width()
-        self.pos=Vector(WIDTH/4,HEIGHT-100)
+        self.pos=pos
         self.used=False
 
     def draw(self,canvas):
         if self.used==False:
             canvas.draw_image(IMAGE, (self.width/2,self.height/2),(self.width,self.height),self.pos.get_p(),(50,50))
+
 '''                          
 hp=HealthPack()     
 frame = simplegui.create_frame('Testing', WIDTH, HEIGHT)
